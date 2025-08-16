c3f10a8a493db7a1d850a9987acf2e1e1c786904
thomas5564 <thomas.chooyankai@gmail.com>
2025-08-16 21:27:55 +0800

    Improve the skeletal version of Avo so that it echos commands entered by the user, and exits when the user types the command bye

--- src/main/java/Avo.java
+++ src/main/java/Avo.java
@@ -1,24 +1,39 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Avo {
     public static void greet(){
-        String greetString =
-                """
-                        ____________________________________________________________
-                         Hello! I'm Avo
-                         What can I do for you?
-                        ____________________________________________________________
-                """;
-        System.out.println(greetString);
+        String greetString = "Hello! I'm Avo \n         What can I do for you?";
+        System.out.println(borderfy(greetString));
     }
     public static void bye(){
-        String byeString =
+        String byeString = "Bye. Hope to see you again soon!";
+        System.out.println(borderfy(byeString));
+    }
+    public static String borderfy(String s){
+        String output = String.format(
                 """
-                        Bye. Hope to see you again soon!
                         ____________________________________________________________
-                """;
-        System.out.println(byeString);
+                         %s
+                        ____________________________________________________________
+                """,s);
+        return output;
+    }
+    public static void respond(Scanner scanner,String response){
+        String borderfiedResponse = borderfy(response);
+        System.out.println(borderfiedResponse);
     }
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         greet();
-        bye();
+        while(true){
+            String input = scanner.nextLine();
+            if (!Objects.equals(input, "bye")){
+                respond(scanner,input);
+            }else{
+                bye();
+                break;
+            }
+        }
     }
 }
