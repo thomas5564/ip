733c52c7f598f6632d8f46fb536d2f1c1b0c24fa
thomas5564 <thomas.chooyankai@gmail.com>
2025-08-16 21:49:33 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested

--- src/main/java/Avo.java
+++ src/main/java/Avo.java
@@ -2,6 +2,8 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Avo {
+    private static int instructionIndex = 0;
+    private static String[] instructions = new String[100];
     public static void greet(){
         String greetString = "Hello! I'm Avo \n         What can I do for you?";
         System.out.println(borderfy(greetString));
@@ -23,16 +25,35 @@ public class Avo {
         String borderfiedResponse = borderfy(response);
         System.out.println(borderfiedResponse);
     }
+    public static String listString(String[] instructions){
+        StringBuilder listString = new StringBuilder();
+        for(int i = 0; i< instructions.length;i++){
+            if(instructions[i] == null){
+                break;
+            }
+            String line = (i+1) + ". " + instructions[i] + "\n";
+            if (i > 0){
+                line = "         " + line;
+            }
+            listString.append(line);
+        }
+        return listString.toString();
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         greet();
         while(true){
             String input = scanner.nextLine();
-            if (!Objects.equals(input, "bye")){
-                respond(scanner,input);
-            }else{
+            if (Objects.equals(input, "bye")){
                 bye();
                 break;
+            } else if (Objects.equals(input, "list")) {
+                System.out.println(borderfy(listString(instructions)));
+            } else{
+                instructions[instructionIndex] = input;
+                instructionIndex++;
+                String fullResponse = "added: " + input;
+                respond(scanner,fullResponse);
             }
         }
     }
