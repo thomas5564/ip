5064bf41834a26548c873134698ce02108763fdf
thomas5564 <thomas.chooyankai@gmail.com>
2025-08-20 01:32:41 +0800

    Restored relative file path

--- src/main/java/main/Avo.java
+++ src/main/java/main/Avo.java
@@ -3,10 +3,11 @@ import Storage.Storage;
 import Ui.Ui;
 import Tasks.TaskList;
 
+import java.io.File;
+
 public class Avo {
     //C:\Users\thoma\Downloads\ip\data\avo.txt
-    //"data" + File.separator + "avo.txt"
-    public static String pathName = "C:\\Users\\thoma\\Downloads\\ip\\data\\avo.txt";
+    public static String pathName = "data" + File.separator + "avo.txt";
     public static Storage storage = new Storage(pathName);
     public static TaskList taskList = new TaskList();
     public static void main(String[] args){
