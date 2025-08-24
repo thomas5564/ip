d32c02b2322490cf95a4416b8232831879c5c45d
thomas5564 <thomas.chooyankai@gmail.com>
2025-08-24 21:28:58 +0800

    Resolve styling issues

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -4,6 +4,7 @@ import Exceptions.EmptyInstructionException;
 import org.junit.jupiter.api.Test;
 import tasks.Deadline;
 import tasks.Event;
+import tasks.Task;
 
 import java.time.LocalDate;
 
@@ -11,12 +12,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserTest {
     @Test
-    public void eventParserTest() throws EmptyInstructionException {
-
-    }
-
-    @Test
-    public void parseStorageStringTest(){
-        assertEquals(4, 4);
+    public void TasksParserTest() throws EmptyInstructionException {
+        Event event = Parser.parseEvent("event play games /from 2003-02-07 /to 2004-12-21");
+        assertEquals(event.getStorageString(), "E|o|play games|2003-02-07|2004-12-21");
+        Deadline deadline = Parser.parseDeadline("deadline play games /by 2003-02-07");
+        assertEquals(deadline.getStorageString(), "D|o|play games|2003-02-07");
+        Task task = Parser.parseTask("todo play games");
+        assertEquals(task.getStorageString(), "T|o|play games");
     }
 }
