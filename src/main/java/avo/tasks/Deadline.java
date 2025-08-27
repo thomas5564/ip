de8a9513975ee13bb0cb1f89f1c5cdfeb0af019e
thomas5564 <thomas.chooyankai@gmail.com>
2025-08-27 12:43:31 +0800

    Fix styling issues for all classes

--- src/main/java/avo/tasks/Deadline.java
+++ src/main/java/avo/tasks/Deadline.java
@@ -3,28 +3,37 @@ package avo.tasks;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Class for deadlines
+ */
 public class Deadline extends Task {
     private LocalDate deadline;
-    public Deadline(String instruction,LocalDate deadline){
+
+    /**
+     * Constructor for this class
+     * @param instruction instruction for task
+     * @param deadline date time object that represents deadline
+     */
+    public Deadline(String instruction, LocalDate deadline) {
         super(instruction);
         this.deadline = deadline;
     }
     @Override
-    public String toString(){
+    public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
         String formattedDate = deadline.format(formatter);
         String deadline = String.format("(by: %s)", formattedDate);
-        return String.format("[D]%s%s",super.toString(),deadline);
+        return String.format("[D]%s%s", super.toString(), deadline);
     }
 
     /**
-     *
+     * Produces string representation of deadline for storage
      * @return String representation of deadline in the storage file
      */
     @Override
-    public String getStorageString(){
-        return String.format("D|%s|%s"
-                ,super.getStorageString().substring(2)
-                ,deadline);
+    public String getStorageString() {
+        return String.format("D|%s|%s",
+                super.getStorageString().substring(2),
+                deadline);
     }
 }
--- src/main/java/avo/tasks/Event.java
+++ src/main/java/avo/tasks/Event.java
@@ -3,31 +3,42 @@ package avo.tasks;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Class to represent tasks with a start and end time
+ */
 public class Event extends Task {
     private final LocalDate startTime;
     private final LocalDate endTime;
-    public Event(String instruction,LocalDate startTime,LocalDate endTime) {
+
+    /**
+     * Constructor for this class
+     * @param instruction instruction for event
+     * @param startTime starting time for the event
+     * @param endTime ending time for the event
+     */
+    public Event(String instruction, LocalDate startTime, LocalDate endTime) {
         super(instruction);
         this.startTime = startTime;
         this.endTime = endTime;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
         String formattedStartTime = startTime.format(formatter);
         String formattedEndTime = endTime.format(formatter);
-        String duration = String.format("(from: %s to: %s)",formattedStartTime,formattedEndTime);
-        return String.format("[E]%s%s",super.toString(),duration);
+        String duration = String.format("(from: %s to: %s)", formattedStartTime, formattedEndTime);
+        return String.format("[E]%s%s", super.toString(), duration);
     }
 
     /**
-     *
+     * Produces string representation of event for storage
      * @return String representation of event in the storage file
      */
     @Override
-    public String getStorageString(){
-        return String.format("E|%s|%s|%s",super.getStorageString().substring(2),
+    public String getStorageString() {
+        return String.format("E|%s|%s|%s",
+                super.getStorageString().substring(2),
                 startTime,
                 endTime);
     }
