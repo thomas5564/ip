56d9b7b55f3bac855e34b97ff5c4ff0e524b9675
thomas5564 <thomas.chooyankai@gmail.com>
2025-08-27 12:09:16 +0800

    Fix styling issues in the exceptions package

--- src/main/java/avo/exceptions/EmptySearchStringException.java
+++ src/main/java/avo/exceptions/EmptySearchStringException.java
@@ -1,5 +1,8 @@
 package avo.exceptions;
 
+/**
+ * Exception for when the searched string is empty
+ */
 public class EmptySearchStringException extends AvoException {
     public EmptySearchStringException() {
         super("You have to be finding something :d");
--- src/main/java/avo/exceptions/UnknownCommandException.java
+++ src/main/java/avo/exceptions/UnknownCommandException.java
@@ -1,5 +1,8 @@
 package avo.exceptions;
 
+/**
+ * Excpetion for whenn the user inputs incorrect commands
+ */
 public class UnknownCommandException extends AvoException {
     public UnknownCommandException() {
         super("OOPS!!! I'm sorry, but I don't know what that means :-(");
