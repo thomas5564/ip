f23546a7ff6b16b07b4b773109f9d3a532c5e693
thomas5564 <thomas.chooyankai@gmail.com>
2025-08-28 01:01:28 +0800

    Implement basic user input handling, echo the user's input back to him

--- src/main/java/avo/ui/MainWindow.java
+++ src/main/java/avo/ui/MainWindow.java
@@ -1,14 +1,47 @@
 package avo.ui;
 
+import java.util.Objects;
+
 import avo.main.Avo;
+import avo.main.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+
 
 /**
  * Root node of application
  */
 public class MainWindow extends AnchorPane {
     private Avo avo;
+    private Image avoImage = new Image(Objects.requireNonNull(Main.class.getResourceAsStream("/images/avo.jpg")));
+    private Image userImage = new Image(Objects.requireNonNull(Main.class.getResourceAsStream("/images/user.jpg")));
+
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private Button sendButton;
+    @FXML
+    private TextField userInput;
     public void setAvo(Avo avo) {
         this.avo = avo;
+    }
+
+    /**
+     * Handle user inputs from the textfield and reflect it in the dialogbox
+     */
+    public void handleUserInput() {
+        String input = userInput.getText();
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(input, avoImage)
+        );
     }
 }
